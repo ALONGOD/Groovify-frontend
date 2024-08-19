bb79d44dc9322d536eb7463b966ac6cdb27ad0b7
ALONGOD <alongoddard1@gmail.com>
2024-08-19 13:37:58 +0300

    Urgent commit (no battery)

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -3,12 +3,12 @@ import { Routes, Route } from 'react-router'
 
 import { HomePage } from './pages/HomePage'
 import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
-import { CarIndex } from './pages/CarIndex.jsx'
+import { StationIndex } from './pages/StationIndex.jsx'
 import { ReviewIndex } from './pages/ReviewIndex.jsx'
 import { ChatApp } from './pages/Chat.jsx'
 import { AdminIndex } from './pages/AdminIndex.jsx'
 
-import { CarDetails } from './pages/CarDetails'
+import { StationDetails } from './pages/StationDetails'
 import { UserDetails } from './pages/UserDetails'
 
 import { AppHeader } from './cmps/AppHeader'
@@ -20,24 +20,24 @@ import { Signup } from './pages/Signup.jsx'
 
 
 export function RootCmp() {
-    return (
-      <div className="app">
-        <MenuSidebar />
-        <DetailsSidebar />
-        <UserMsg />
-        
-        <main>
-          <AppHeader />
-          <Routes>
-            <Route path="/" element={<AppIndex />}>
-              <Route path="/home" element={<Homepage />} />
-              <Route path="/liked" element={<LikedSongsStation />} />
-              <Route path="/station/:id" element={<StationList />} />
-              <Route path="/search" element={<Search />} />
-            </Route>
-          </Routes>
-        </main>
-        <AppFooter />
-      </div>
-    )
-  }
+  return (
+    <div className="app">
+      <MenuSidebar />
+      <DetailsSidebar />
+      <UserMsg />
+
+      <main>
+        <AppHeader />
+        <Routes>
+          <Route path="/" element={<AppIndex />}>
+            <Route path="/home" element={<Homepage />} />
+            <Route path="/liked" element={<LikedSongsStation />} />
+            <Route path="/station/:id" element={<StationList />} />
+            <Route path="/search" element={<Search />} />
+          </Route>
+        </Routes>
+      </main>
+      <AppFooter />
+    </div>
+  )
+}
