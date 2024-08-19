3641e89bba3e300d50452b951f5f0416e9299c3f
ALONGOD <alongoddard1@gmail.com>
2024-08-19 19:37:11 +0300

    Use local service in station actions

--- src/services/station/station.service.local.js
+++ src/services/station/station.service.local.js
@@ -1,87 +1,96 @@
 
-import { storageService } from '../async-storage.service'
-import { makeId } from '../util.service'
-import { userService } from '../user'
+import { storageService } from '../async-storage.service';
+import { makeId } from '../util.service';
+import { userService } from '../user';
+import { stations as demoStations, user as demoUser } from '../../../demo_data/station.js';
 
-const STORAGE_KEY = 'station'
+const STORAGE_KEY = 'station';
 
 export const stationService = {
     query,
     getById,
     save,
     remove,
-    addStationMsg
-}
-window.cs = stationService
+    addStationMsg,
+    initializeDemoData,  // Add this method
+};
 
+window.cs = stationService;
+
+initializeDemoData(); // Call this function on initialization
+
+async function initializeDemoData() {
+    let stations = await storageService.query(STORAGE_KEY);
+    if (!stations || !stations.length) {
+        stations = demoStations;
+        await storageService.post(STORAGE_KEY, stations);
+        console.log('Demo stations initialized in local storage.');
+    }
+}
 
 async function query(filterBy = { txt: '', price: 0 }) {
-    var stations = await storageService.query(STORAGE_KEY)
-    const { txt, minSpeed, maxPrice, sortField, sortDir } = filterBy
+    var stations = await storageService.query(STORAGE_KEY);
+    const { txt, minSpeed, maxPrice, sortField, sortDir } = filterBy;
 
     if (txt) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        stations = stations.filter(station => regex.test(station.vendor) || regex.test(station.description))
+        const regex = new RegExp(filterBy.txt, 'i');
+        stations = stations.filter(station => regex.test(station.vendor) || regex.test(station.description));
     }
     if (minSpeed) {
-        stations = stations.filter(station => station.speed >= minSpeed)
+        stations = stations.filter(station => station.speed >= minSpeed);
     }
     if (sortField === 'vendor' || sortField === 'owner') {
         stations.sort((station1, station2) =>
-            station1[sortField].localeCompare(station2[sortField]) * +sortDir)
+            station1[sortField].localeCompare(station2[sortField]) * +sortDir);
     }
     if (sortField === 'price' || sortField === 'speed') {
         stations.sort((station1, station2) =>
-            (station1[sortField] - station2[sortField]) * +sortDir)
+            (station1[sortField] - station2[sortField]) * +sortDir);
     }
 
-    stations = stations.map(({ _id, vendor, price, speed, owner }) => ({ _id, vendor, price, speed, owner }))
-    return stations
+    stations = stations.map(({ _id, vendor, price, speed, owner }) => ({ _id, vendor, price, speed, owner }));
+    return stations;
 }
 
 function getById(stationId) {
-    return storageService.get(STORAGE_KEY, stationId)
+    return storageService.get(STORAGE_KEY, stationId);
 }
 
 async function remove(stationId) {
-    // throw new Error('Nope')
-    await storageService.remove(STORAGE_KEY, stationId)
+    await storageService.remove(STORAGE_KEY, stationId);
 }
 
 async function save(station) {
-    var savedStation
+    var savedStation;
     if (station._id) {
         const stationToSave = {
             _id: station._id,
             price: station.price,
             speed: station.speed,
-        }
-        savedStation = await storageService.put(STORAGE_KEY, stationToSave)
+        };
+        savedStation = await storageService.put(STORAGE_KEY, stationToSave);
     } else {
         const stationToSave = {
             vendor: station.vendor,
             price: station.price,
             speed: station.speed,
-            // Later, owner is set by the backend
             owner: userService.getLoggedinUser(),
             msgs: []
-        }
-        savedStation = await storageService.post(STORAGE_KEY, stationToSave)
+        };
+        savedStation = await storageService.post(STORAGE_KEY, stationToSave);
     }
-    return savedStation
+    return savedStation;
 }
 
 async function addStationMsg(stationId, txt) {
-    // Later, this is all done by the backend
-    const station = await getById(stationId)
-
+    const station = await getById(stationId);
     const msg = {
         id: makeId(),
         by: userService.getLoggedinUser(),
         txt
-    }
-    station.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, station)
+    };
+    station.msgs.push(msg);
+    await storageService.put(STORAGE_KEY, station);
 
-    return msg
+    return msg;
 }
