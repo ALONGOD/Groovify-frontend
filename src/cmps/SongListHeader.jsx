a389ad43cd7574ad1937d23cb8e816b6734698e3
aidqen <idanmarkin8@gmail.com>
2024-08-19 22:10:21 +0300

    Add SongPreview and SongListHeader to StationDetails

---
+++ src/cmps/SongListHeader.jsx
@@ -0,0 +1,12 @@
+export function SongListHeader() {
+     
+    return (
+        <div className="song-header flex">
+            <h4>#</h4>
+            <h4>Title</h4>
+            <h4>Album</h4>
+            <h4>Added At</h4>
+            <h4>Duration</h4>
+        </div>
+    )
+}
--- src/pages/StationDetails.jsx
+++ src/pages/StationDetails.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+
 import { stationService } from '../services/station/station.service.local'
+import { SongPreview } from '../cmps/SongPreview'
+import { SongListHeader } from '../cmps/SongListHeader'
 
 export function StationDetails() {
   const { stationId } = useParams()
   const [station, setStation] = useState({})
+
   console.log('station:', station.songs)
 
   useEffect(() => {
@@ -22,23 +24,19 @@ export function StationDetails() {
       throw err
     }
   }
+
   if (!station) return <h1>Loading...</h1>
   return (
     <section className="station-details">
-      <ul className="flex flex-column">
+      <ul className="song-list flex flex-column">
+        <SongListHeader />
+        <hr class="custom-divider" />
         {station.songs &&
           station.songs.map((song, idx) => {
             return (
-              <li className="flex flex-row align-center" key={song.id}>
-                <h4>{idx + 1}</h4>
-                <img src={song.imgUrl} alt="song-img" />
-                <div className="song-details flex flex-column">
-                  <h4>{song.title}</h4>
-                  <h4>{song.album ? song.album : 'Unknown'}</h4>
-                </div>
-                <h4>{song.addedAt}</h4>
-                <h4>{song.duration}</h4>
-              </li>
+              
+                <SongPreview song={song} idx={idx} key={song.id}/>
+              
             )
           })}
       </ul>
