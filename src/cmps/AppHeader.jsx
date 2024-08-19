2b7ce90cb188ca3c941bf73dda0fb729c6f78ce9
aidqen <idanmarkin8@gmail.com>
2024-08-19 19:37:58 +0300

    Finish styling AppHeader

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,8 +1,10 @@
 import { Link, NavLink, useSearchParams } from 'react-router-dom'
-import {  useLocation, useNavigate } from 'react-router'
+import { useLocation, useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 import { FaRegBell } from 'react-icons/fa'
+import { FaMagnifyingGlass } from 'react-icons/fa6'
+import { useState } from 'react'
 
 export function AppHeader() {
   const user = useSelector(storeState => storeState.userModule.user)
@@ -13,17 +15,30 @@ export function AppHeader() {
 
   return (
     <header className="app-header flex flex-row justify-between align-center">
-      <div className="right flex flex-row align-center">
-        <IoIosArrowBack />
-        <IoIosArrowForward />
-        {isSearchPage && <input
-          type="text"
-          name="search"
-          placeholder="What do you want to play?"
-        />}
-      </div>
       <div className="left flex flex-row align-center">
-        <FaRegBell />
+        <div className="action-nav-btns flex flex-row">
+          <div className="react-icon" >
+            <IoIosArrowBack title='Go back'/>
+          </div>
+          <div className="react-icon" >
+            <IoIosArrowForward title='Go forward'/>
+          </div>
+        </div>
+        {isSearchPage && (
+          <div className='search-bar'>
+            <FaMagnifyingGlass/>
+            <input
+              type="text"
+              name="search"
+              placeholder="What do you want to play?"
+            />
+          </div>
+        )}
+      </div>
+      <div className="right flex flex-row align-center">
+        <div className="react-icon">
+          <FaRegBell />
+        </div>
         <img
           src="https://i.scdn.co/image/ab67757000003b8212291183e9c0be882978f504"
           className="rounded-full"
