e4f870cbe2e11d5639bfdd2e6509c82f68de7338
aidqen <idanmarkin8@gmail.com>
2024-08-19 13:12:20 +0300

    Add RootCmp routes

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -18,34 +18,26 @@ import { LoginSignup } from './pages/LoginSignup.jsx'
 import { Login } from './pages/Login.jsx'
 import { Signup } from './pages/Signup.jsx'
 
+
 export function RootCmp() {
     return (
-        <div className="main-container">
-            <AppHeader />
-            <UserMsg />
-
-            <main>
-                <Routes>
-                    <Route path="" element={<HomePage />} />
-                    <Route path="about" element={<AboutUs />}>
-                        <Route path="team" element={<AboutTeam />} />
-                        <Route path="vision" element={<AboutVision />} />
-                    </Route>
-                    <Route path="car" element={<CarIndex />} />
-                    <Route path="car/:carId" element={<CarDetails />} />
-                    <Route path="user/:id" element={<UserDetails />} />
-                    <Route path="review" element={<ReviewIndex />} />
-                    <Route path="chat" element={<ChatApp />} />
-                    <Route path="admin" element={<AdminIndex />} />
-                    <Route path="login" element={<LoginSignup />}>
-                        <Route index element={<Login />} />
-                        <Route path="signup" element={<Signup />} />
-                    </Route>
-                </Routes>
-            </main>
-            <AppFooter />
-        </div>
+      <div className="app">
+        <MenuSidebar />
+        <DetailsSidebar />
+        <UserMsg />
+        
+        <main>
+          <AppHeader />
+          <Routes>
+            <Route path="/" element={<AppIndex />}>
+              <Route path="/home" element={<Homepage />} />
+              <Route path="/liked" element={<LikedSongsStation />} />
+              <Route path="/station/:id" element={<StationList />} />
+              <Route path="/search" element={<Search />} />
+            </Route>
+          </Routes>
+        </main>
+        <AppFooter />
+      </div>
     )
-}
-
-
+  }
