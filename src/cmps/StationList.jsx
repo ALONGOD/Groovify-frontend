ab8912354d06ac15bf2d192dd5696c067600cd92
aidqen <idanmarkin8@gmail.com>
2024-08-20 13:53:27 +0300

    Add StationPreview and StationList

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -1,27 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { userService } from '../services/user'
 import { StationPreview } from './StationPreview'
 import { stationService } from '../services/station/station.service.local'
 
 export function StationList() {
-    const [stations, setStations] = useState([])
+  const [stations, setStations] = useState([])
 
-    useEffect(() => {
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-    }, [])
-    
-    async function fetchData() {
-
+  async function fetchData() {
+    try {
+      const stations = await stationService.query()
+      setStations(stations)
+    } catch (err) {
+      console.log('Cannot load stations', err)
+      throw err
     }
+  }
 
+  console.log(stations)
 
-    return <section>
-        <ul className="list">
-            {stations.map(station =>
-                <li key={station._id}>
-                    <StationPreview station={station} />
-                </li>)
-            }
-        </ul>
+  if (!stations) return <h1>Loading...</h1>
+  return (
+    <section className="station-list">
+      <ul>
+        {stations.map(station => (
+          <StationPreview station={station} key={station._id} />
+        ))}
+      </ul>
     </section>
+  )
 }
--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,10 +1,20 @@
-import { Link } from 'react-router-dom'
+import { BsFillPinAngleFill } from 'react-icons/bs'
 
 export function StationPreview({ station }) {
-    return <article className="preview">
-        <header>
-            <Link to={`/station/${station._id}`}></Link>
-        </header>
-
-    </article>
+  return (
+    <li className="station-preview flex flex-row">
+      <img src={station.imgUrl} alt="station img" />
+      <div className="flex flex-column">
+        <h3>{station.name}</h3>
+        <div className='station-details flex flex-row'>
+          <div>
+            <BsFillPinAngleFill />
+          </div>
+          <span>Playlist</span>
+          <span className='divider'>&#9679;</span>
+          <span>{station.songs.length} songs</span>
+        </div>
+      </div>
+    </li>
+  )
 }
