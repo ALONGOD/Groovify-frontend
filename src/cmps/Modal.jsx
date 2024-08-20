56f644cdb2da641d869db6f084be11af52269098
aidqen <idanmarkin8@gmail.com>
2024-08-20 22:35:53 +0300

    Add Modal skeleton and styling

--- src/cmps/Modal.jsx
+++ src/cmps/Modal.jsx
@@ -1,23 +1,26 @@
+import { AiOutlinePlus } from 'react-icons/ai'
 import { FaCaretRight, FaPlus, FaRegTrashAlt } from 'react-icons/fa'
 import { FiPlusCircle } from 'react-icons/fi'
+import { GoPlus, GoPlusCircle } from 'react-icons/go'
 
 export function Modal() {
   return (
     <>
-      <div className="backdrop"></div>
-      <div className="modal">
+      <div className="modal absolute">
         <div className="modal-content">
-          <div className="flex flex-row">
-            <FaPlus />
-            <h3>Add to playlist</h3>
+          <div className="row flex flex-row justify-between">
+            <div className="flex flex-row">
+              <AiOutlinePlus />
+              <h3>Add to playlist</h3>
+            </div>
             <FaCaretRight />
           </div>
-          <div className="flex flex-row">
+          <div className="row flex flex-row">
             <FaRegTrashAlt />
             <h3>Remove from playlist</h3>
           </div>
-          <div className="flex flex-row">
-            <FiPlusCircle />
+          <div className="row flex flex-row">
+            <GoPlusCircle />
             <h3>Save to your liked songs</h3>
           </div>
         </div>
