e6f074ea63b1623d6391f327db98b65bc416698d
Ran <ranglikman@gmail.com>
2024-08-20 10:41:10 +0300

    Add library to sidebar and change icons

--- src/cmps/MenuSidebar.jsx
+++ src/cmps/MenuSidebar.jsx
@@ -1,18 +1,42 @@
 import { NavLink } from 'react-router-dom'
-import { FaHome, FaSearch } from 'react-icons/fa'
+import { VscLibrary } from 'react-icons/vsc'
+import { GoHomeFill, GoPlus, GoArrowRight } from 'react-icons/go'
+import { FiSearch } from 'react-icons/fi'
 
 export function MenuSidebar() {
-    return (
-        <aside className='menu-sidebar flex flex-column'>
-            <NavLink to='/' activeClassName='active' className='nav-link flex'>
-                <FaHome className='icon mr-4 text-2xl' />
+    const MainMenu = () => (
+        <div className='main-menu flex flex-column'>
+            <NavLink
+                to='/'
+                className={({ isActive }) => `nav-link flex ${isActive ? 'active' : ''}`}
+            >
+                <GoHomeFill className='icon text-2xl' />
                 <span>Home</span>
             </NavLink>
 
-            <NavLink to='/search' activeClassName='active' className='nav-link flex'>
-                <FaSearch className='icon mr-4 text-2xl' />
+            <NavLink
+                to='/search'
+                className={({ isActive }) => `nav-link flex ${isActive ? 'active' : ''}`}
+            >
+                <FiSearch className='icon text-2xl' />
                 <span>Search</span>
             </NavLink>
+        </div>
+    )
+
+    const LibraryMenu = () => (
+        <div className='library-menu nav-link flex'>
+            <VscLibrary className='icon text-2xl' />
+            <span>Your Library</span>
+            <GoPlus className='ml-auto icon text-2xl' />
+            <GoArrowRight className='icon text-2xl' />
+        </div>
+    )
+
+    return (
+        <aside className='menu-sidebar flex flex-column'>
+            <MainMenu />
+            <LibraryMenu />
         </aside>
     )
 }
