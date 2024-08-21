637bfd0c56fb5eacd57c508746cd40fff0e96f4d
aidqen <idanmarkin8@gmail.com>
2024-08-21 21:13:54 +0300

    Fix SongList design diversity problem

--- src/cmps/SongList.jsx
+++ src/cmps/SongList.jsx
@@ -1,26 +1,38 @@
-import { useSelector } from "react-redux";
-import { removeSongFromStation, toggleModal } from "../store/actions/station.actions";
-import { SongPreview } from "./StationDetails/SongPreview";
+import { useSelector } from 'react-redux'
+import {
+  removeSongFromStation,
+  toggleModal,
+} from '../store/actions/station.actions'
+import { SongPreview } from './StationDetails/SongPreview'
+import { SongListHeader } from './StationDetails/SongListHeader'
 
-export function SongList({ songs }) {
-    const songModal = useSelector(state => state.stationModule.modalSong)
+export function SongList({ songs, type }) {
+  const songModal = useSelector(state => state.stationModule.modalSong)
 
-    function onToggleModal(event, song) {
-        event.stopPropagation();
-        toggleModal(song)
-      }
+  function onToggleModal(event, song) {
+    event.stopPropagation()
+    toggleModal(song)
+  }
 
-      
-
-  return songs.map((song, idx) => {
-    return (
-      <SongPreview
-        key={song.id}
-        song={song}
-        idx={idx}
-        songModal={songModal}
-        onToggleModal={onToggleModal}
-      />
-    )
-  })
+  return (
+    <ul className="song-list flex flex-column">
+      {type === 'list-table' && (
+        <>
+          <SongListHeader />
+          <hr className="custom-divider" />
+        </>
+      )}
+      {songs.map((song, idx) => {
+        return (
+          <SongPreview
+            key={song.id}
+            song={song}
+            idx={idx}
+            songModal={songModal}
+            onToggleModal={onToggleModal}
+          />
+        )
+      })}
+    </ul>
+  )
 }
