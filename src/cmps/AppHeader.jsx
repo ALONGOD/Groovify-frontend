09b4bb5b27adb0321d3ddb83b461471a4a343efb
ALONGOD <alongoddard1@gmail.com>
2024-08-21 11:39:10 +0300

    Make separate search bar cmp

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -4,41 +4,13 @@ import { FaRegBell } from 'react-icons/fa'
 import { FaMagnifyingGlass } from 'react-icons/fa6'
 import { YouTubeAPIService } from '../services/youtubeAPI/fetchYoutubeApi.js'
 import { useState, useCallback } from 'react';
+import { SearchBar } from './SearchBar.jsx'
+
 
 export function AppHeader() {
   const navigate = useNavigate()
   const location = useLocation()
   const isSearchPage = location.pathname.includes('/search')
-  const [searchTerm, setSearchTerm] = useState('');
-
-  function debounce(func, delay) {
-    let timer;
-    return function (...args) {
-      if (timer) clearTimeout(timer);
-      timer = setTimeout(() => {
-        func(...args);
-      }, delay);
-    };
-  }
-
-  const handleChange = (event) => {
-    const value = event.target.value;
-    setSearchTerm(value);
-    handleSearch(value);
-  };
-  const handleSearch = useCallback(
-    debounce(async (query) => {
-      if (query) {
-        try {
-          const results = await YouTubeAPIService.searchVideos(query);
-          console.log(results);
-        } catch (error) {
-          console.error('Error fetching YouTube API:', error);
-        }
-      }
-    }, 800), // Adjust the delay as needed (500ms in this example)
-    []
-  );
 
   function navigation(direction) {
     navigate(direction)
@@ -57,14 +29,7 @@ export function AppHeader() {
         </div>
         {isSearchPage && (
           <div className='search-bar'>
-            <FaMagnifyingGlass />
-            <input
-              type="text"
-              name="search"
-              placeholder="What do you want to play?"
-              value={searchTerm}
-              onChange={handleChange}
-            />
+            <SearchBar />
           </div>
         )}
       </div>
