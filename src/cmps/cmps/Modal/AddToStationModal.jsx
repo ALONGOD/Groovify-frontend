8e4a0246d2b5309e9ff6fde830714f7cf0325f44
aidqen <idanmarkin8@gmail.com>
2024-08-21 13:58:43 +0300

    Change PlaylistsModal name

---
+++ src/cmps/cmps/Modal/AddToStationModal.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { FaMagnifyingGlass } from 'react-icons/fa6'
+import { useSelector } from 'react-redux'
+import { addToStation } from '../../../store/actions/station.actions'
+
+export function AddToStationModal({ stations }) {
+  const songModal = useSelector(state => state.stationModule.modalSong)
+  const [newPlaylist, setNewPlaylist] = useState({ name: '' })
+  console.log(stations)
+
+  function handleChange({ target }) {
+    const { value } = target
+    setNewPlaylist({ ...newPlaylist, name: value })
+  }
+
+  function onAddToStation(stationId) {
+    addToStation(stationId, songModal)
+  }
+
+  return (
+    <>
+      <div className="searchbar">
+        <FaMagnifyingGlass />
+        <input
+          type="text"
+          name="name"
+          id=""
+          onClick={e => e.stopPropagation()}
+          value={newPlaylist.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="row flex flex-row">
+        <AiOutlinePlus />
+        <h3>Add new playlist</h3>
+      </div>
+      {stations.map(station => {
+        return (
+          <h3 className="row" key={station._id} onClick={() => onAddToStation(station._id)}>
+            {station.name}
+          </h3>
+        )
+      })}
+    </>
+  )
+}
--- src/cmps/cmps/Modal/Modal.jsx
+++ src/cmps/cmps/Modal/Modal.jsx
@@ -4,31 +4,29 @@ import { AiOutlinePlus } from 'react-icons/ai'
 import { FaCaretRight, FaPlus, FaRegTrashAlt } from 'react-icons/fa'
 import { FiPlusCircle } from 'react-icons/fi'
 import { GoPlus, GoPlusCircle } from 'react-icons/go'
-import { PlaylistsModal } from './PlaylistsModal'
 import { ModalSongOptions } from './ModalSongOptions'
+import { AddToStationModal } from './AddToStationModal'
 
 export function Modal({ modalType }) {
   const stations = useSelector(state => state.stationModule.stations)
-
 
   return (
     <>
       <div className={`modal ${modalType === 'playlists' ? 'playlist-modal' : ''} absolute`}>
         <div className="modal-content">
-          <DynamicModal modalType={modalType} stations={stations}/>
+          <DynamicModal modalType={modalType} stations={stations} />
         </div>
       </div>
     </>
   )
 }
 
-
 function DynamicModal({ modalType, stations }) {
   switch (modalType) {
     case 'songOptions':
       return <ModalSongOptions />
     case 'playlists':
-      return <PlaylistsModal stations={stations}/>
+      return <AddToStationModal stations={stations}/>
 
   }
   
