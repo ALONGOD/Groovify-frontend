7030589baed37a918a7afdc56eddd974cb682da3
Ran <ranglikman@gmail.com>
2024-08-23 13:57:39 +0300

    Use currSong to show playing song details on details bar

--- src/cmps/DetailsSidebar.jsx
+++ src/cmps/DetailsSidebar.jsx
@@ -1,44 +1,26 @@
-import { useSelector } from "react-redux"
+import { useSelector } from 'react-redux'
 
 export function DetailsSidebar() {
+    const currSong = useSelector(state => state.stationModule.currSong)
+
     return (
         <aside className={`details-sidebar`}>
             <header className='details-header'>
-                <h3>AC/DC</h3>
+                <h3>{currSong ? currSong.artist : 'Metro Boomin, A$AP Rocky, Takeoff'}</h3>
                 <div className='header-actions'>
                     <button className='more-options'>•••</button>
                     <button className='close-btn'>×</button>
                 </div>
             </header>
             <div className='details-image'>
-                <img
-                    src='https://images.squarespace-cdn.com/content/v1/5f4a6763a23dbb3ab86255f7/1714746991313-X8OIYB1KK2ZR01U15XK4/30.png'
-                    alt='Album Art'
-                />
+                <img src={currSong?.imgUrl || 'https://i.ytimg.com/vi/WwSRA2p4He0/mqdefault.jpg'} alt='Album Art' />
             </div>
             <div className='details-song-info'>
                 <div className='song-and-band'>
-                    <h2>You Shook Me All Night Long</h2>
-                    <h4>AC/DC</h4>
+                    <h2>{currSong ? currSong.title : 'Feel the Fiyaaah (with A$AP Rocky & feat. Takeoff)'}</h2>
+                    <h4>{currSong ? currSong.artist : 'Metro Boomin, A$AP Rocky, Takeoff'}</h4>
                 </div>
                 <button className='add-btn'>+</button>
-            </div>
-
-            <div className='artist-info'>
-                <h4>About the artist</h4>
-                <div className='artist-details'>
-                    <img src='path-to-artist-image.jpg' alt='AC/DC' />
-                    <div>
-                        <p>AC/DC</p>
-                        <p>39,792,675 monthly listeners</p>
-                        <p>
-                            With a limitless supply of dirty riffs, snarling vocals, and timelessly
-                            catchy, anthemic choruses, AC/DC is one of the most important and
-                            long-lasting forces in hard rock.
-                        </p>
-                        <button className='unfollow-btn'>Unfollow</button>
-                    </div>
-                </div>
             </div>
         </aside>
     )
