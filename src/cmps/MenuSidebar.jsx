b2b513c2c7315f442b3fa844ee748c3ea0b045b9
Ran <ranglikman@gmail.com>
2024-08-24 19:12:37 +0300

    Auto collapse menu sidebar when below 1232px width

--- src/cmps/MenuSidebar.jsx
+++ src/cmps/MenuSidebar.jsx
@@ -7,42 +7,49 @@ import { useDispatch, useSelector } from 'react-redux'
 import { SET_USER } from '../store/reducers/user.reducer'
 
 export function MenuSidebar() {
-  const dispatch = useDispatch()
-  const user = useSelector(state => state.userModule.user)
-  console.log('user:', user)
-  const [isCollapsed, setIsCollapsed] = useState(false)
-  const [selected, setSelected] = useState(null)
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.userModule.user)
+    console.log('user:', user)
+    const [isCollapsed, setIsCollapsed] = useState(false)
+    const [selected, setSelected] = useState(null)
 
-  useEffect(() => {
-    getUser()
-  }, [])
-  
-  async function getUser() {
-    console.log('hi');
-    
-    const user = await userService.getLoggedinUser()
-    dispatch({type: SET_USER, user})
-  }
-  
+    useEffect(() => {
+        getUser()
+        window.addEventListener('resize', handleResize)
+        handleResize()
 
-  return (
-    <aside
-      className={`menu-sidebar flex flex-column ${
-        isCollapsed ? 'collapsed' : ''
-      }`}
-    >
-      <div className='navigation'>
-        <MainMenu isCollapsed={isCollapsed} />
-      </div>
-      <div className='library-menu flex flex-column'>
-        <LibraryMenu
-          isCollapsed={isCollapsed}
-          setIsCollapsed={setIsCollapsed}
-          selected={selected}
-          setSelected={setSelected}
-        />
-        <StationList isCollapsed={isCollapsed}/>
-      </div>
-    </aside>
-  )
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+    async function getUser() {
+        const user = await userService.getLoggedinUser()
+        dispatch({ type: SET_USER, user })
+    }
+
+    function handleResize() {
+        if (window.innerWidth < 1232) {
+            setIsCollapsed(true)
+        } else {
+            setIsCollapsed(false)
+        }
+    }
+
+    return (
+        <aside className={`menu-sidebar flex flex-column ${isCollapsed ? 'collapsed' : ''}`}>
+            <div className='navigation'>
+                <MainMenu isCollapsed={isCollapsed} />
+            </div>
+            <div className='library-menu flex flex-column'>
+                <LibraryMenu
+                    isCollapsed={isCollapsed}
+                    setIsCollapsed={setIsCollapsed}
+                    selected={selected}
+                    setSelected={setSelected}
+                />
+                <StationList isCollapsed={isCollapsed} />
+            </div>
+        </aside>
+    )
 }
