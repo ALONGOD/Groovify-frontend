53b89b045d18b90075f036ddc4ada42f4998cb74
Ran <ranglikman@gmail.com>
2024-08-26 00:05:14 +0300

    Add "Add to Liked Songs" button on footer

--- src/cmps/AppFooter/AppFooter.jsx
+++ src/cmps/AppFooter/AppFooter.jsx
@@ -1,27 +1,26 @@
 import { FaCircleCheck } from 'react-icons/fa6'
 import { MusicPlayer } from './MusicPlayer'
 import { useSelector } from 'react-redux'
-
+import { LikeSongBtn } from '../LikeSongBtn'
 
 export function AppFooter() {
-  const currSong = useSelector(storeState => storeState.stationModule.currSong)
+    const currSong = useSelector(storeState => storeState.stationModule.currSong)
 
-  return (
-    <footer className="app-footer full flex flex-row align-center justify-between">
-      <div className="details flex flex-row align-center">
-        {currSong && (
-          <>
-            <img src={currSong.imgUrl} alt="song-img" />
-            <div className="flex flex-column">
-              <h3>{currSong.title}</h3>
-              <h4>{currSong.artist}</h4>
+    return (
+        <footer className='app-footer full flex flex-row align-center justify-between'>
+            <div className='details flex flex-row align-center'>
+                {currSong && (
+                    <>
+                        <img src={currSong.imgUrl} alt='song-img' />
+                        <div className='flex flex-column'>
+                            <h3>{currSong.title}</h3>
+                            <h4>{currSong.artist}</h4>
+                        </div>
+                        <LikeSongBtn song={currSong} />{' '}
+                    </>
+                )}
             </div>
-            <FaCircleCheck />
-          </>
-        )}
-      </div>
-      <MusicPlayer currSong={currSong} />
-      
-    </footer>
-  )
+            <MusicPlayer currSong={currSong} />
+        </footer>
+    )
 }
--- src/cmps/LikeSongBtn.jsx
+++ src/cmps/LikeSongBtn.jsx
@@ -1,11 +1,10 @@
-import { CiCirclePlus } from "react-icons/ci";
-import { IoIosCheckmarkCircle } from "react-icons/io";
-import { addToLikedSongs, removeFromLikedSongs } from "../store/actions/station.actions";
-import { useSelector } from "react-redux";
+import { CiCirclePlus } from 'react-icons/ci'
+import { IoIosCheckmarkCircle } from 'react-icons/io'
+import { addToLikedSongs, removeFromLikedSongs } from '../store/actions/station.actions'
+import { useSelector } from 'react-redux'
 
 export function LikeSongBtn({ onHover, song }) {
     const stations = useSelector(state => state.stationModule.stations)
-    // const stationDetails = useSelector(state => state.stationModule.stationDetails)
     const likedSongs = stations.find(station => station._id === 'liked-songs')?.songs
     const isSongLiked = likedSongs.some(likedSong => likedSong.id === song.id)
 
@@ -18,10 +17,20 @@ export function LikeSongBtn({ onHover, song }) {
     }
 
     return (
-        <div className="like-song-container flex align-center justify-start">
-            {isSongLiked ? 
-            <IoIosCheckmarkCircle className={`like-song-btn liked ${onHover ? '' : 'hidden'}`} onClick={onRemoveFromLikedSongs}/> :
-            <CiCirclePlus  className={`like-song-btn ${onHover ? '' : 'hidden'}`} onClick={onAddToLikedSongs} />}
+        <div className='like-song-container flex align-center justify-start'>
+            {isSongLiked ? (
+                <IoIosCheckmarkCircle
+                    className='like-song-btn liked'
+                    onClick={onRemoveFromLikedSongs}
+                    title='Remove from Liked Songs'
+                />
+            ) : (
+                <CiCirclePlus
+                    className='like-song-btn'
+                    onClick={onAddToLikedSongs}
+                    title='Add to Liked Songs'
+                />
+            )}
         </div>
     )
 }
