4c4ea4d5c077970acca2460b9a397d03d17416b4
aidqen <idanmarkin8@gmail.com>
2024-08-26 13:37:29 +0300

    Add ImagePick to EditStationModal

--- src/cmps/ImagePick.jsx
+++ src/cmps/ImagePick.jsx
@@ -1,10 +1,8 @@
 import { useRef, useState } from 'react'
+import { LuPen } from 'react-icons/lu'
 
-export function ImagePick({ setDetailsToEdit, pickedImg }) {
-//   const [pickedImage, setPickedImage] = useState()
+export function ImagePick({ setStation, pickedImg }) {
   const [onHoverImage, setOnHoverImage] = useState(false)
-  
-
   const imageInput = useRef()
 
   function handlePickClick() {
@@ -14,49 +12,42 @@ export function ImagePick({ setDetailsToEdit, pickedImg }) {
   function handleImageChange({ target }) {
     const file = target.files[0]
     if (!file) {
-    //   setPickedImage(null)
-      setDetailsToEdit((prev) => ({ ...prev, img: null }))
+      setStation(prev => ({ ...prev, imgUrl: null }))
       return
     }
 
     const fileReader = new FileReader()
 
     fileReader.onload = () => {
-    //   setPickedImage(fileReader.result)
-      setDetailsToEdit((prev) => ({ ...prev, img: fileReader.result }))
+      setStation(prev => ({ ...prev, imgUrl: fileReader.result }))
     }
 
     fileReader.readAsDataURL(file)
   }
 
   return (
-    <div className="flex flex-row items-center justify-between bg-background-mainBg text-slate-500 w-full h-[11em] p-4 mb-8 rounded-xl">
-      <h4>Profile Picture</h4>
+    <div className="img relative">
       {pickedImg ? (
         <div
           onMouseOver={() => setOnHoverImage(true)}
           onMouseLeave={() => setOnHoverImage(false)}
-          className="relative w-[8em] h-full rounded-lg cursor-pointer  bg-purple-light transition-all duration-200"
+          className=""
           onClick={handlePickClick}
         >
-          <p
-            className={`${
-              onHoverImage ? 'block' : 'hidden'
-            } z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  text-purple-bright font-bold`}
-          >
-            Change Photo
-          </p>
+          {onHoverImage && (
+            <div className="hover-container flex flex-column absolute align-center justify-center">
+              <LuPen />
+              <p>Choose Photo</p>
+            </div>
+          )}
           <img
             src={pickedImg}
             alt="The image selected by the user"
-            className={`w-full h-full rounded-lg hover:opacity-50 hover:blur-sm transition-all duration-200`}
+            className={``}
           />
         </div>
       ) : (
-        <div
-          onClick={handlePickClick}
-          className="flex flex-col bg-purple-light text-purple-bright justify-center items-center p-4 rounded-xl h-full min-w-[8em] max-w-[8em] font-bold gap-4 cursor-pointer hover:text-slate-700 hover:bg-purple-200 duration-200"
-        >
+        <div onClick={handlePickClick} className="">
           <i className="fa-regular fa-image fa-xl"></i>
           <h4 className="text-[0.8em]">+ Upload Image</h4>
         </div>
@@ -69,10 +60,6 @@ export function ImagePick({ setDetailsToEdit, pickedImg }) {
         ref={imageInput}
         onChange={handleImageChange}
       />
-      <div className="flex flex-col">
-        <h5>Image must be below 1024x1024px</h5>
-        <h5>Use PNG or JPG format</h5>
-      </div>
     </div>
   )
 }
