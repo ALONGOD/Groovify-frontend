05ff7b23687845f4a076878f940da1567a3fe6b5
Ran <ranglikman@gmail.com>
2024-08-28 16:25:52 +0300

    Move components into clearer divs, starting 2024 design of Spotify

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,8 +1,8 @@
+// src/RootCmp.jsx
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { StationDetails } from './pages/StationDetails'
-
 import { AppFooter } from './cmps/AppFooter/AppFooter.jsx'
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { MenuSidebar } from './cmps/MenuSidebar.jsx'
@@ -11,24 +11,30 @@ import { AppIndex } from './pages/AppIndex.jsx'
 import { Homepage } from './pages/HomePage.jsx'
 import { SearchPage } from './pages/SearchPage.jsx'
 import { toggleModal } from './store/actions/station.actions.js'
+import { AppHeader } from './cmps/AppHeader'
 
 export function RootCmp() {
     return (
         <div className='app' onClick={toggleModal}>
-            <MenuSidebar />
-            <UserMsg />
+            <AppHeader />
+            <div className='content-wrapper'>
+                <MenuSidebar />
 
-            <Routes>
-                <Route path='/' element={<AppIndex />}>
-                    <Route path='/' element={<Homepage />} />
-                    <Route path='/station/:stationId' element={<StationDetails />} />
-                    <Route path='/search' element={<SearchPage />} />
-                    <Route path='/search/:searchTerm' element={<SearchPage />} />
-                </Route>
-            </Routes>
-            <DetailsSidebar />
+                <main className='main-content'>
+                    <Routes>
+                        <Route path='/' element={<AppIndex />}>
+                            <Route path='/' element={<Homepage />} />
+                            <Route path='/station/:stationId' element={<StationDetails />} />
+                            <Route path='/search' element={<SearchPage />} />
+                            <Route path='/search/:searchTerm' element={<SearchPage />} />
+                        </Route>
+                    </Routes>
+                </main>
 
+                <DetailsSidebar />
+            </div>
             <AppFooter />
+            <UserMsg />
         </div>
     )
 }
