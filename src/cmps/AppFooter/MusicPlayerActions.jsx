853680c10509bb56679df548932bb8ede22e97d2
aidqen <idanmarkin8@gmail.com>
2024-08-28 17:27:28 +0300

    Add dynamic DetailsSidebar and minimal QueueDetails

--- src/cmps/AppFooter/MusicPlayerActions.jsx
+++ src/cmps/AppFooter/MusicPlayerActions.jsx
@@ -5,6 +5,7 @@ import { ProgressBar } from "./ProgressBar";
 import { useEffect, useState } from "react";
 import { TOGGLE_DETAILS_SIDEBAR } from "../../store/reducers/system.reducer";
 import { useDispatch, useSelector } from "react-redux";
+import { setDetailsSidebar } from "../../store/actions/system.actions";
 
 export function MusicPlayerActions({volume, setVolume, handleVolumeChange, toggleDetailsSidebar, isVolumeMuted, toggleVolume, playerRef
 }) {
@@ -20,19 +21,18 @@ useEffect(() => {
   setIsActive(isDetailsOpen)
 }, [isDetailsOpen])
 
-
-function toggleDetailsSidebar() {
+function onSetDetailsSidebar(state) {
   setIsActive(prevState => !prevState)
-  dispatch({ type: TOGGLE_DETAILS_SIDEBAR, state: 'songDetails' })
+  setDetailsSidebar(state)
 }
-  
+
   return (
     <div className="other-options flex flex-row align-center">
       <IoPlayCircleOutline
-        onClick={toggleDetailsSidebar}
+        onClick={() => onSetDetailsSidebar('songDetails')}
         style={{ color: isActive ? '#00ba5c' : 'inherit' }}
       />
-      <HiOutlineQueueList />
+      <HiOutlineQueueList onClick={() => onSetDetailsSidebar('queueDetails')}/>
       {isVolumeMuted ? <AiOutlineMuted onClick={toggleVolume}/> : <AiOutlineSound onClick={toggleVolume}/>}
       <ProgressBar currProgress={volumeToSave} setCurrent={setVolume} maxProgress={100} handleProgressClick={handleVolumeChange} type='volume-progress' playerRef={playerRef} />
     </div>
--- src/cmps/DetailsSidebar/DetailsSidebar.jsx
+++ src/cmps/DetailsSidebar/DetailsSidebar.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { DetailsSidebarClose } from './DetailsSidebarClose'
 import { TOGGLE_DETAILS_SIDEBAR } from '../../store/reducers/system.reducer'
-import { LikeSongBtn } from '../LikeSongBtn'
+import { SongDetails } from './SongDetails'
+import { QueueDetails } from './QueueDetails'
 
 export function DetailsSidebar() {
     const currSong = useSelector(state => state.stationModule.currSong)
     const isDetailsOpen = useSelector(state => state.systemModule.isDetailsOpen)
+    // console.log('isDetailsOpen:', isDetailsOpen)
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -24,30 +25,8 @@ export function DetailsSidebar() {
 
     return (
         <aside className={`details-sidebar ${isDetailsOpen ? 'open' : 'closed'}`}>
-            <header className='details-header'>
-                <h3>{currSong ? currSong.artist : 'Metro Boomin, A$AP Rocky, Takeoff'}</h3>
-                <div className='header-actions'>
-                    <button className='more-options'>•••</button>
-                    <DetailsSidebarClose />
-                </div>
-            </header>
-            <div className='details-image'>
-                <img
-                    src={currSong?.imgUrl || 'https://i.ytimg.com/vi/WwSRA2p4He0/mqdefault.jpg'}
-                    alt='Album Art'
-                />
-            </div>
-            <div className='details-song-info'>
-                <div className='song-and-band'>
-                    <h2>
-                        {currSong
-                            ? currSong.title
-                            : 'Feel the Fiyaaah (with A$AP Rocky & feat. Takeoff)'}
-                    </h2>
-                    <h4>{currSong ? currSong.artist : 'Metro Boomin, A$AP Rocky, Takeoff'}</h4>
-                </div>
-                <LikeSongBtn song={currSong} />
-            </div>
+           {isDetailsOpen === 'songDetails' && <SongDetails currSong={currSong} />}
+           {isDetailsOpen === 'queueDetails' && <QueueDetails />}
         </aside>
     )
 }
