789dd0b206620aa4e13aea9755ac6905261f377d
Ran <ranglikman@gmail.com>
2024-08-28 22:21:13 +0300

    Add mouse scroll support to progress and volume slider bars, refactor code

--- src/cmps/AppFooter/ProgressBar.jsx
+++ src/cmps/AppFooter/ProgressBar.jsx
@@ -1,74 +1,127 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from 'react'
 
-export function ProgressBar({ currProgress, setCurrent, maxProgress, handleProgressClick, type, playerRef }) {
-
+export function ProgressBar({
+    currProgress,
+    setCurrent,
+    maxProgress,
+    handleProgressClick,
+    type,
+    playerRef,
+}) {
     const [progress, setProgress] = useState((currProgress / maxProgress) * 100 || 0)
     const [onHover, setOnHover] = useState(false)
     const [isDragging, setIsDragging] = useState(false)
-    const progressContainerRef = useRef(null);
+    const progressContainerRef = useRef(null)
     const isVolume = type === 'volume-progress'
 
     useEffect(() => {
-      setProgress((currProgress / maxProgress) * 100)
+        setProgress((currProgress / maxProgress) * 100)
     }, [currProgress, maxProgress])
-    
-    
 
     useEffect(() => {
         if (isDragging) {
-            window.addEventListener('mousemove', handleMouseMove);
-            window.addEventListener('mouseup', handleMouseUp);
+            window.addEventListener('mousemove', handleMouseMove)
+            window.addEventListener('mouseup', handleMouseUp)
         } else {
-            window.removeEventListener('mousemove', handleMouseMove);
-            window.removeEventListener('mouseup', handleMouseUp);
+            window.removeEventListener('mousemove', handleMouseMove)
+            window.removeEventListener('mouseup', handleMouseUp)
         }
 
         return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-            window.removeEventListener('mouseup', handleMouseUp);
-        };
-    }, [isDragging]);
+            window.removeEventListener('mousemove', handleMouseMove)
+            window.removeEventListener('mouseup', handleMouseUp)
+        }
+    }, [isDragging])
 
+    function onUpdateProgress(e = null, newValue = null) {
+        const progressContainer = progressContainerRef.current
+        const width = progressContainer.offsetWidth
+        let maxValue, calculatedValue
 
-    function onUpdateProgress(e) {
-        const progressContainer = progressContainerRef.current;
-        const width = progressContainer.offsetWidth;
-        const clickX = e.clientX - progressContainer.getBoundingClientRect().left;
         if (isVolume) {
-            var maxValue = 100;
-            var newValue = Math.max(0, Math.min((clickX / width) * maxValue, maxValue));
-            playerRef.current.setVolume(newValue);
+            maxValue = 100
+            calculatedValue =
+                newValue !== null
+                    ? newValue
+                    : Math.max(
+                          0,
+                          Math.min(
+                              ((e.clientX - progressContainer.getBoundingClientRect().left) /
+                                  width) *
+                                  maxValue,
+                              maxValue
+                          )
+                      )
+            playerRef.current.setVolume(calculatedValue)
         } else {
-            var maxValue = playerRef.current.getDuration();
-            var newValue = Math.max(0, Math.min((clickX / width) * maxValue, maxValue));
-            playerRef.current.seekTo(newValue, true);
+            maxValue = playerRef.current.getDuration()
+            calculatedValue =
+                newValue !== null
+                    ? newValue
+                    : Math.max(
+                          0,
+                          Math.min(
+                              ((e.clientX - progressContainer.getBoundingClientRect().left) /
+                                  width) *
+                                  maxValue,
+                              maxValue
+                          )
+                      )
+            playerRef.current.seekTo(calculatedValue, true)
         }
 
-        setCurrent(newValue)
-        setProgress((newValue / maxValue) * 100);
-    };
+        setCurrent(calculatedValue)
+        setProgress((calculatedValue / maxValue) * 100)
+    }
 
     function handleMouseDown(e) {
-        setIsDragging(true);
-        onUpdateProgress(e);
-    };
+        setIsDragging(true)
+        onUpdateProgress(e)
+    }
 
     function handleMouseMove(e) {
         if (isDragging) {
-            onUpdateProgress(e);
+            onUpdateProgress(e)
         }
-    };
-
-    function handleMouseUp() {
-        setIsDragging(false);
     }
 
-    return <div className={`clickable-area ${type}`} ref={progressContainerRef} onMouseDown={handleMouseDown}  onMouseEnter={() => setOnHover(true)} onMouseLeave={() => setOnHover(false)}>
-        <div className='progress-container' onClick={handleProgressClick}>
-            <div className='progress-bar' style={{ width: `${progress}%` }} >
-                {onHover && <div className='progress-thumb' style={{ right: 0 }}></div>}
+    function handleMouseUp() {
+        setIsDragging(false)
+    }
+
+    function handleWheel(e) {
+        e.preventDefault()
+        let delta
+        if (isVolume) {
+            delta = e.deltaY < 0 ? 5 : -5
+            const currentVolume = playerRef.current.getVolume()
+            const newVolume = Math.max(0, Math.min(currentVolume + delta, 100))
+            onUpdateProgress(null, newVolume)
+        } else {
+            delta = e.deltaY > 0 ? 5 : -5
+            const currentTime = playerRef.current.getCurrentTime()
+            const newTime = Math.max(
+                0,
+                Math.min(currentTime - delta, playerRef.current.getDuration())
+            )
+            onUpdateProgress(null, newTime)
+        }
+    }
+
+    return (
+        <div
+            className={`clickable-area ${type}`}
+            ref={progressContainerRef}
+            onMouseDown={handleMouseDown}
+            onMouseEnter={() => setOnHover(true)}
+            onMouseLeave={() => setOnHover(false)}
+            onWheel={handleWheel}
+        >
+            <div className='progress-container' onClick={handleProgressClick}>
+                <div className='progress-bar' style={{ width: `${progress}%` }}>
+                    {onHover && <div className='progress-thumb' style={{ right: 0 }}></div>}
+                </div>
             </div>
         </div>
-    </div>
-
+    )
 }
