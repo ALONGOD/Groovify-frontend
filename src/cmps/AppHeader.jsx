379b450dd984949714da46a3ddc3944ffd627a19
Ran <ranglikman@gmail.com>
2024-08-30 11:10:22 +0300

    Add elements of 08/2024 redesign

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,8 +1,8 @@
 import { useLocation, useNavigate } from 'react-router'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 import { FaRegBell, FaSpotify } from 'react-icons/fa'
+import { GoHomeFill, GoHome } from 'react-icons/go'
 import { FaMagnifyingGlass } from 'react-icons/fa6'
-import { YouTubeAPIService } from '../services/youtubeAPI/fetchYoutubeApi.js'
 import { useState, useCallback } from 'react'
 import { SearchBar } from './SearchBar.jsx'
 import { NavLink } from 'react-router-dom'
@@ -22,23 +22,27 @@ export function AppHeader() {
             {/* Left Div: Spotify Icon, Back, and Forward Buttons */}
             <div className='left flex flex-row align-center'>
                 <NavLink to='/'>
-                    <FaSpotify size={30} color='#1DB954' /> {/* NavLink wrapped around the icon */}
+                    <FaSpotify size={30} color='white' />
                 </NavLink>
+
                 <div className='action-nav-btns flex flex-row'>
                     <div className='react-icon'>
                         <IoIosArrowBack title='Go back' onClick={() => navigation(-1)} />
                     </div>
+
                     <div className='react-icon'>
                         <IoIosArrowForward title='Go forward' onClick={() => navigation(1)} />
                     </div>
                 </div>
             </div>
 
-            {/* Middle Div: Search Bar */}
+            {/* Middle Div: Home Button and Search Bar */}
             <div className='middle flex flex-row align-center'>
+                <NavLink to='/' className='home-button'>
+                    {({ isActive }) => <>{isActive ? <GoHomeFill /> : <GoHome />}</>}
+                </NavLink>
+
                 <NavLink to='/search' className='search-bar'>
-                    {' '}
-                    {/* NavLink added for the search bar */}
                     <SearchBar searchType={'youtube'} />
                 </NavLink>
             </div>
