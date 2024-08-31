853227af961375682ea8218d8c57c10a6a8eee80
aidqen <idanmarkin8@gmail.com>
2024-08-31 17:07:05 +0300

    Add EqualizerBar without implementation yet

---
+++ src/cmps/EqualizerBar.jsx
@@ -0,0 +1,11 @@
+export function EqualizerBar(params) {
+  return (
+    <div class="equalizer">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+  )
+}
