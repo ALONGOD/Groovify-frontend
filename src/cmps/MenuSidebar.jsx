ecf148aa6afa277bf9723ef45143dcce2c82b80d
Ran <ranglikman@gmail.com>
2024-09-01 13:18:19 +0300

    Change menu sidebar resizing and collapsing parameters

--- src/cmps/MenuSidebar.jsx
+++ src/cmps/MenuSidebar.jsx
@@ -1,39 +1,39 @@
-import { useEffect, useState } from 'react';
-import { MainMenu } from './MainMenu';
-import { LibraryMenu } from './LibraryMenu';
-import { StationList } from './StationList';
-import { userService } from '../services/user/user.service.local';
-import { useDispatch, useSelector } from 'react-redux';
-import { SET_USER } from '../store/reducers/user.reducer';
+import { useEffect, useState } from 'react'
+import { MainMenu } from './MainMenu'
+import { LibraryMenu } from './LibraryMenu'
+import { StationList } from './StationList'
+import { userService } from '../services/user/user.service.local'
+import { useDispatch, useSelector } from 'react-redux'
+import { SET_USER } from '../store/reducers/user.reducer'
 
 export function MenuSidebar() {
-    const dispatch = useDispatch();
-    const user = useSelector(state => state.userModule.user);
-    const [isCollapsed, setIsCollapsed] = useState(false);
-    const [isBelowThreshold, setIsBelowThreshold] = useState(false);
-    const [selected, setSelected] = useState(null);
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.userModule.user)
+    const [isCollapsed, setIsCollapsed] = useState(false)
+    const [isBelowThreshold, setIsBelowThreshold] = useState(false)
+    const [selected, setSelected] = useState(null)
 
     useEffect(() => {
-        getUser();
-        window.addEventListener('resize', handleResize);
-        handleResize();
+        getUser()
+        window.addEventListener('resize', handleResize)
+        handleResize()
 
         return () => {
-            window.removeEventListener('resize', handleResize);
+            window.removeEventListener('resize', handleResize)
         }
-    }, []);
+    }, [])
 
     async function getUser() {
-        const user = await userService.getLoggedinUser();
-        dispatch({ type: SET_USER, user });
+        const user = await userService.getLoggedinUser()
+        dispatch({ type: SET_USER, user })
     }
 
     function handleResize() {
-        if (window.innerWidth < 815) {
-            setIsCollapsed(true);
-            setIsBelowThreshold(true);
+        if (window.innerWidth < 1100) {
+            setIsCollapsed(true)
+            setIsBelowThreshold(true)
         } else {
-            setIsBelowThreshold(false);
+            setIsBelowThreshold(false)
         }
     }
 
@@ -50,5 +50,5 @@ export function MenuSidebar() {
                 <StationList isCollapsed={isCollapsed} user={user} />
             </div>
         </aside>
-    );
+    )
 }
