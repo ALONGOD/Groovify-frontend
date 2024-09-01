943b393c7433cdd4c69864b92331ee1e6fc23eaf
aidqen <idanmarkin8@gmail.com>
2024-09-01 20:45:01 +0300

    Implement PlayPauseBtn

--- src/cmps/SearchPage/TopResult.jsx
+++ src/cmps/SearchPage/TopResult.jsx
@@ -1,11 +1,19 @@
+import { PlayPauseBtn } from "../PlayPauseBtn";
+
 export function TopResult({ topResult }) {
   return (
     <div className="top-result flex flex-column">
       <h2>Top Result</h2>
-      <div className="card flex flex-column">
+      <div className="card relative flex flex-column">
         <img src={topResult?.imgUrl} alt="top-result" />
-        <h3>{topResult?.title}</h3>
-        <p>{topResult?.artist}</p>
+        <div className="details">
+          <h3>{topResult?.title}</h3>
+          <div className="flex flex-row align-center">
+            <p className="song-label">Song •</p>
+            <p>{topResult?.artist}</p>
+          </div>
+        </div>
+        <PlayPauseBtn song={topResult} type="top-result"/>
       </div>
     </div>
   )
