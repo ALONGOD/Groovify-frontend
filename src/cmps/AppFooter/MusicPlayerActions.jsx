28b1aeb46c3abd36831f25c39e8f92b860af2919
aidqen <idanmarkin8@gmail.com>
2024-09-03 13:33:03 +0300

    Add svgs and apply to MusicPlayerActions and LibraryMenu

--- src/cmps/AppFooter/MusicPlayerActions.jsx
+++ src/cmps/AppFooter/MusicPlayerActions.jsx
@@ -1,4 +1,4 @@
-import { AiOutlineAudioMuted, AiOutlineMuted, AiOutlineSound } from "react-icons/ai";
+import { AiOutlineAudioMuted, AiOutlineMuted, AiOutlinePlaySquare, AiOutlineSound } from "react-icons/ai";
 import { HiOutlineQueueList } from "react-icons/hi2";
 import { IoPlayCircleOutline } from "react-icons/io5";
 import { ProgressBar } from "./ProgressBar";
@@ -6,6 +6,10 @@ import { useEffect, useState } from "react";
 import { TOGGLE_DETAILS_SIDEBAR } from "../../store/reducers/system.reducer";
 import { useDispatch, useSelector } from "react-redux";
 import { setDetailsSidebar } from "../../store/actions/system.actions";
+import { NoVolume } from "../svgs/NoVolume";
+import { MaxVolume } from "../svgs/MaxVolume";
+import { QueueButton } from "../svgs/QueueButton";
+// import {ReactComponent as LibraryIcon} from '../../assets/svgs/your-library.svg'
 
 export function MusicPlayerActions({volume, setVolume, handleVolumeChange, toggleDetailsSidebar, isVolumeMuted, toggleVolume, playerRef
 }) {
@@ -24,12 +28,12 @@ function onSetDetailsSidebar(state) {
 
   return (
     <div className="other-options flex flex-row align-center">
-      <IoPlayCircleOutline
+      <AiOutlinePlaySquare
         onClick={() => onSetDetailsSidebar('songDetails')}
         style={{ color: detailsSidebarMode === 'songDetails' ? '#00ba5c' : 'inherit' }}
       />
-      <HiOutlineQueueList onClick={() => onSetDetailsSidebar('queueDetails')} style={{ color: detailsSidebarMode === 'queueDetails' ? '#00ba5c' : 'inherit' }}/>
-      {isVolumeMuted ? <AiOutlineMuted onClick={toggleVolume}/> : <AiOutlineSound onClick={toggleVolume}/>}
+      <QueueButton onClick={() => onSetDetailsSidebar('queueDetails')} style={{ fill: detailsSidebarMode === 'queueDetails' ? '#00ba5c' : 'white' }}/>
+      {isVolumeMuted ? <NoVolume onClick={toggleVolume}/> : <MaxVolume onClick={toggleVolume}/>}
       <ProgressBar currProgress={volumeToSave} setCurrent={setVolume} maxProgress={100} handleProgressClick={handleVolumeChange} type='volume-progress' playerRef={playerRef} />
     </div>
   )
