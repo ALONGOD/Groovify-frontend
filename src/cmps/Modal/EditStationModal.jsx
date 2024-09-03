9c27246d9cd9ff92768975c56a714443d2540a59
aidqen <idanmarkin8@gmail.com>
2024-09-03 15:59:32 +0300

    Fix editStation modal and pixel perfect it

--- src/cmps/Modal/EditStationModal.jsx
+++ src/cmps/Modal/EditStationModal.jsx
@@ -1,7 +1,10 @@
 import { useParams } from 'react-router'
 import { stationService } from '../../services/station/station.service.local'
 import { useEffect, useState } from 'react'
-import { SET_EDIT_MODAL, UPDATE_STATION } from '../../store/reducers/station.reducer'
+import {
+  SET_EDIT_MODAL,
+  UPDATE_STATION,
+} from '../../store/reducers/station.reducer'
 import { useDispatch } from 'react-redux'
 import { storageService } from '../../services/async-storage.service'
 import { ImagePick } from '../ImagePick'
@@ -10,13 +13,13 @@ export function EditStationModal() {
   const dispatch = useDispatch()
   const params = useParams()
   const [station, setStation] = useState({})
-  
+
   console.log('station:', station)
 
   useEffect(() => {
     fetchStation()
   }, [])
-  
+
   async function fetchStation() {
     const stationToSet = await stationService.getById(params.stationId)
     setStation(stationToSet)
@@ -30,7 +33,7 @@ export function EditStationModal() {
   function handleSave(ev) {
     ev.preventDefault()
     storageService.put('stationDB', station)
-    dispatch({ type: UPDATE_STATION, updatedStation: station})
+    dispatch({ type: UPDATE_STATION, updatedStation: station })
     closeEditModal()
   }
 
@@ -44,43 +47,43 @@ export function EditStationModal() {
         <h2>Edit details</h2>
         <button onClick={closeEditModal}>x</button>
       </div>
-
-      <div className="main-details flex flex-row">
-        <ImagePick setStation={setStation} pickedImg={station.imgUrl}/>
-        {/* <img
-          src={
-            station?.imgUrl
-              ? station?.imgUrl
-              : 'https://st4.depositphotos.com/9998432/22597/v/450/depositphotos_225976914-stock-illustration-person-gray-photo-placeholder-man.jpg'
-          }
-          alt="Station Img"
-        /> */}
-        <div className="inputs flex flex-column">
-          <div className="input-with-label">
-            <label>Name</label>
-            <input type="text" value={station.name} onChange={handleChange} name='name' placeholder="Add a name" />
-          </div>
-          <div className="input-with-label textarea">
-            <label htmlFor="">Description</label>
-            <textarea
-              type="text"
-              value={station.description}
-              onChange={handleChange} name='description'
-              placeholder="Add an optional description"
-            />
+      <main>
+        <div className="main-details flex flex-row">
+          <ImagePick setStation={setStation} pickedImg={station.imgUrl} />
+          <div className="inputs flex flex-column">
+            <div className="input-with-label">
+              <label>Name</label>
+              <input
+                type="text"
+                value={station.name}
+                onChange={handleChange}
+                name="name"
+                placeholder="Add a name"
+              />
+            </div>
+            <div className="input-with-label textarea">
+              <label htmlFor="">Description</label>
+              <textarea
+                type="text"
+                value={station.description}
+                onChange={handleChange}
+                name="description"
+                placeholder="Add an optional description"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="save-container flex justify-end align-center">
-        <button className="save-btn">Save</button>
-      </div>
-      <div className="description flex flex-column justify-start">
-        <p>
-          By proceeding, you agree to give Spotify access to the image you
-          choose to upload.
-        </p>
-        <p>Please make sure you have the right to upload the image.</p>
-      </div>
+        <div className="save-container flex justify-end align-center">
+          <button className="save-btn">Save</button>
+        </div>
+        <div className="description flex flex-column justify-start">
+          <p>
+            By proceeding, you agree to give Spotify access to the image you
+            choose to upload.
+          </p>
+          <p>Please make sure you have the right to upload the image.</p>
+        </div>
+      </main>
     </form>
   )
 }
