a362808f28e6aeeb88c117e1456c60d853552526
ALONGOD <alongoddard1@gmail.com>
2024-09-09 11:41:20 +0300

    Add check for artist image and fix height

--- src/cmps/ArtistList.jsx
+++ src/cmps/ArtistList.jsx
@@ -1,18 +1,21 @@
-// cmps/ArtistList.jsx
 import React from 'react';
 
 export function ArtistList({ artists }) {
     return (
         <div className="artist-list">
-            {artists.map((artist) => (
-                <div key={artist.id} className="artist-item">
-                    <img src={artist.images[0]?.url || 'default-image-url'} alt={artist.name} />
-                    <div className="artist-info">
-                        <h3>{artist.name}</h3>
-                        <p>{artist.followers.total.toLocaleString()} followers</p>
+            {artists.map((artist) => {
+                if (artist.images.length === 0) return null; // Correct use of conditional
+
+                return (
+                    <div key={artist.id} className="artist-item">
+                        <img src={artist.images[0]?.url || 'default-image-url'} alt={artist.name} />
+                        <div className="artist-info">
+                            <h3>{artist.name}</h3>
+                            <p>{artist.followers.total.toLocaleString()} followers</p>
+                        </div>
                     </div>
-                </div>
-            ))}
+                );
+            })}
         </div>
     );
 }
