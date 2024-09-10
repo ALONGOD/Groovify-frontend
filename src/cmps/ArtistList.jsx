cf1911025b1e236558f26797c340ac78f8165434
aidqen <idanmarkin8@gmail.com>
2024-09-10 22:29:59 +0300

    Navigation to ArtistDetails from ArtistList

--- src/cmps/ArtistList.jsx
+++ src/cmps/ArtistList.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 export function ArtistList({ artists }) {
+    const navigate = useNavigate();
+
+    function navigateToArtist(artistId) {
+        navigate(`/artist/${artistId}`);
+    }
+    
     return (
         <div className="artist-list">
             {artists.map((artist) => {
+                console.log('artist:', artist)
                 if (artist.images.length === 0) return null; // Correct use of conditional
-
                 return (
-                    <div key={artist.id} className="artist-item">
+                    <div key={artist.id} className="artist-item" onClick={() => navigateToArtist(artist.id)}>
                         <img src={artist.images[0]?.url || 'default-image-url'} alt={artist.name} />
                         <div className="artist-info">
                             <h3>{artist.name}</h3>
