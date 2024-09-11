6e70d5da6e750c87e168c901755edd4004a9d971
ALONGOD <alongoddard1@gmail.com>
2024-09-11 14:00:05 +0300

    Add songs DnD (still doesn't update order in DB but able to drag normally)

--- src/cmps/SongList.jsx
+++ src/cmps/SongList.jsx
@@ -2,12 +2,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import { setSongsInQueue, toggleModal } from '../store/actions/station.actions'
 import { SongPreview } from './SongPreview'
 import { SongListHeader } from './StationDetails/SongListHeader'
-import { SearchBar } from './SearchBar'
+import { useEffect, useState } from 'react'
+import update from 'immutability-helper'
 import { SET_SONGS } from '../store/reducers/station.reducer'
-import { useEffect } from 'react'
 
 export function SongList({ songs, type, station }) {
   const dispatch = useDispatch()
+  const [songOrder, setSongOrder] = useState(songs) // Local state for song order
   const songModal = useSelector(state => state.stationModule.modalSong)
   const user = useSelector(state => state.userModule.user)
   const likedSongs = user?.likedSongsStation?.songs
@@ -16,20 +17,29 @@ export function SongList({ songs, type, station }) {
     dispatch({ type: SET_SONGS, songs })
   }, [])
 
+  useEffect(() => {
+    setSongOrder(songs)
+    dispatch({ type: SET_SONGS, songs })
+  }, [songs])
+
   async function onSetSongsInQueue() {
     await setSongsInQueue(songs)
   }
 
-  // console.log(songs)
+  const moveSong = (dragIndex, hoverIndex) => {
+    const draggedSong = songOrder[dragIndex]
+    const updatedOrder = update(songOrder, {
+      $splice: [[dragIndex, 1], [hoverIndex, 0, draggedSong]],
+    })
+    setSongOrder(updatedOrder)
+    dispatch({ type: SET_SONGS, songs: updatedOrder })
+    // Here you can also save the updated order to the backend or local storage
+  }
 
   return (
     <ul className={`song-list ${type}`}>
-      {type === 'list-table' && (
-        <>
-          <SongListHeader />
-        </>
-      )}
-      {songs.map((song, idx) => {
+      {type === 'list-table' && <SongListHeader />}
+      {songOrder.map((song, idx) => {
         return (
           <SongPreview
             station={station}
@@ -37,12 +47,13 @@ export function SongList({ songs, type, station }) {
             song={song}
             type={type}
             idx={idx}
+            moveSong={moveSong} // Pass moveSong function to SongPreview
             songModal={songModal}
             likedSongs={likedSongs}
             onSetSongsInQueue={onSetSongsInQueue}
           />
-        );
+        )
       })}
     </ul>
-  );
+  )
 }
