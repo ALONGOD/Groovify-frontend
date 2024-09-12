aae0c1434df1497ef3aa0ba972726ddf27313d5c
aidqen <idanmarkin8@gmail.com>
2024-09-12 16:47:52 +0300

    Add disfunctional DnD for stations (not working)

--- src/cmps/MenuSidebar.jsx
+++ src/cmps/MenuSidebar.jsx
@@ -8,70 +8,89 @@ import { SET_USER } from '../store/reducers/user.reducer'
 import { useNavigate } from 'react-router'
 
 export function MenuSidebar() {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const user = useSelector(state => state.userModule.user)
-    console.log('user:', user)
-    const [isCollapsed, setIsCollapsed] = useState(false)
-    const [isBelowThreshold, setIsBelowThreshold] = useState(false)
-    const [selected, setSelected] = useState(null)
-    const [likedStations, setLikedStations] = useState([]);
-    
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.userModule.user)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [isBelowThreshold, setIsBelowThreshold] = useState(false)
+  const [selected, setSelected] = useState(null)
+  const [likedStations, setLikedStations] = useState([])
 
-    useEffect(() => {
-        getUser()
-        window.addEventListener('resize', handleResize)
-        handleResize()
+  useEffect(() => {
+    getUser()
+    window.addEventListener('resize', handleResize)
+    handleResize()
 
-        return () => {
-            window.removeEventListener('resize', handleResize)
-        }
-    }, [])
-
-    useEffect(() => {
-        getUser()
-      }, [])
-
-      useEffect(() => {
-        if (user?.likedStations) setLikedStations([user?.likedSongsStation, ...user?.likedStations])
-      }, [user])
-      
-
-    async function getUser() {
-        try {
-            const userToSave = await userService.getLoggedinUser()
-            if (!userToSave) navigate('/auth/login')
-            // const userToSave = await userService.getById(user._id)
-            dispatch({ type: SET_USER, user: userToSave })
-        } catch (err) {
-            navigate('/auth/login')
-            console.log('Cannot set logged in user', err)
-        }
+    return () => {
+      window.removeEventListener('resize', handleResize)
     }
+  }, [])
 
-    function handleResize() {
-        if (window.innerWidth < 1100) {
-            setIsCollapsed(true)
-            setIsBelowThreshold(true)
-        } else {
-            setIsBelowThreshold(false)
-        }
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  useEffect(() => {
+    if (user?.likedStations)
+      setLikedStations([user?.likedSongsStation, ...user?.likedStations])
+  }, [user])
+
+  async function getUser() {
+    try {
+      const userToSave = await userService.getLoggedinUser()
+      if (!userToSave) navigate('/auth/login')
+      // const userToSave = await userService.getById(user._id)
+      dispatch({ type: SET_USER, user: userToSave })
+    } catch (err) {
+      navigate('/auth/login')
+      console.log('Cannot set logged in user', err)
     }
+  }
 
-    return (
-        <aside className={`menu-sidebar flex flex-column ${isCollapsed ? 'collapsed' : ''}`}>
-            <div className='library-menu flex flex-column'>
-                <div className='library-icon flex flex-column'>
-                    <LibraryMenu
-                        isCollapsed={isCollapsed}
-                        setIsCollapsed={setIsCollapsed}
-                        selected={selected}
-                        setSelected={setSelected}
-                        isBelowThreshold={isBelowThreshold}
-                    />
-                </div>
-                <StationList isCollapsed={isCollapsed} stations={likedStations} type='station-preview'/>
-            </div>
-        </aside>
-    )
+  function handleResize() {
+    if (window.innerWidth < 1100) {
+      setIsCollapsed(true)
+      setIsBelowThreshold(true)
+    } else {
+      setIsBelowThreshold(false)
+    }
+  }
+
+  function moveStation(fromIndex, toIndex) {
+    //   console.log('fromIndex:', fromIndex)
+    //   console.log('toIndex:', toIndex)
+//       const updatedStations = [...likedStations]
+//       if (fromIndex === toIndex) return
+//       if (fromIndex === 0 || toIndex === 0) return
+//         const [movedStation] = updatedStations.splice(fromIndex, 1)
+//         updatedStations.splice(toIndex, 0, movedStation)
+//         setLikedStations(updatedStations)
+  }
+
+  return (
+    <aside
+      className={`menu-sidebar flex flex-column ${
+        isCollapsed ? 'collapsed' : ''
+      }`}
+    >
+      <div className="library-menu flex flex-column">
+        <div className="library-icon flex flex-column">
+          <LibraryMenu
+            isCollapsed={isCollapsed}
+            setIsCollapsed={setIsCollapsed}
+            selected={selected}
+            setSelected={setSelected}
+            isBelowThreshold={isBelowThreshold}
+          />
+        </div>
+        <StationList
+          isCollapsed={isCollapsed}
+          //   stations={likedStations}
+          stations={likedStations}
+          type="station-preview"
+          moveStation={moveStation}
+        />
+      </div>
+    </aside>
+  )
 }
--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -1,7 +1,7 @@
 import { BsFillPinAngleFill } from 'react-icons/bs'
 import { useNavigate } from 'react-router'
 import { useDrag, useDrop } from 'react-dnd'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { PlayPauseBtn } from './PlayPauseBtn'
 import {
@@ -13,23 +13,66 @@ import { setSongsInQueue } from '../store/actions/station.actions'
 import { SET_DETAILS_SIDEBAR } from '../store/reducers/system.reducer'
 import { getStationById } from '../store/actions/backend.station'
 
-export function StationPreview({ station, isCollapsed, index, moveStation, type, user }) {
+export function StationPreview({
+  station,
+  isCollapsed,
+  index,
+  moveStation,
+  type,
+  user,
+}) {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const ref = useRef(null)
 
-  const [onHover, setOnHover] = useState(false)
   const player = useSelector(state => state.stationModule.player)
   const { currStation, isPlaying, currSong } = player
 
   const { isShuffled } = useSelector(state => state.stationModule.queue)
-
+  const [isDraggingOver, setIsDraggingOver] = useState(false)
 
   const { _id, id, imgUrl, name, songs } = station
   station.id = _id ? _id : id
 
   const isStationPlaying = currStation?.id === station?.id
 
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'station',
+    item: { id: station.id, index },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }))
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'station',
+    hover: (item, monitor) => {
+      const dragIndex = item.index
+      const hoverIndex = index
+
+      if (dragIndex === hoverIndex) {
+        setIsDraggingOver(false)
+        return
+      }
+      setIsDraggingOver(true)
+    },
+    drop: item => {
+      const dragIndex = item.index
+      const hoverIndex = index
+      console.log('hoverIndex:', hoverIndex)
+
+      moveStation(dragIndex, hoverIndex)
+      setIsDraggingOver(false)
+    },
+    collect: monitor => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }))
+
+  useEffect(() => {
+    if (!isOver) {
+      setIsDraggingOver(false); 
+    }
+  }, [isOver]);
 
   async function playOrPauseStation(ev) {
     ev.stopPropagation()
@@ -55,53 +98,37 @@ export function StationPreview({ station, isCollapsed, index, moveStation, type,
     }
   }
 
-  const [{ isDragging }, drag] = useDrag({
-    type: 'STATION',
-    item: { index },
-    collect: monitor => ({
-      isDragging: monitor.isDragging(),
-    }),
-  })
-
-  const [, drop] = useDrop({
-    accept: 'STATION',
-    hover: draggedItem => {
-      if (draggedItem.index !== index) {
-        moveStation(draggedItem.index, index)
-        draggedItem.index = index
-      }
-    },
-  })
-
-  drag(drop(ref))
-
   return (
     <li
-      ref={ref}
-      className={`station-preview flex flex-row align-center ${onHover ? 'hover' : ''} ${type === 'search-results' ? 'search-results' : ''}`}
+      ref={node => drag(drop(node))}
+      className={`station-preview flex flex-row align-center ${
+        isDraggingOver ? 'dragging' : ''
+      } ${type === 'search-results' ? 'search-results' : ''}`}
       onClick={() => navigate(`/station/${station.id}`)}
-      onMouseEnter={() => setOnHover(true)}
-      onMouseLeave={() => setOnHover(false)}
       style={{ opacity: isDragging ? 0.5 : 1 }}
     >
       <div className="relative">
         <img src={imgUrl} alt="station img" />
-        {onHover && (
-          <PlayPauseBtn
-            type={type}
-            station={station}
-            onTogglePlay={playOrPauseStation}
-          />
-        )}
+        <PlayPauseBtn
+          type={type}
+          station={station}
+          onTogglePlay={playOrPauseStation}
+        />
       </div>
       {!isCollapsed && (
         <div className="flex flex-column">
           <h3 className={isStationPlaying ? 'active' : ''}>{name}</h3>
           <div className="station-details flex flex-row align-center">
-            {station.id === 'liked-songs' && <BsFillPinAngleFill className='pin' />}
+            {station.id === 'liked-songs' && (
+              <BsFillPinAngleFill className="pin" />
+            )}
             <span>Playlist</span>
             <span className="divider">&#9679;</span>
-            <span>{type === 'userDetails' ? `By ${user.username}` : `${songs?.length ? songs?.length : ''} songs`} </span>
+            <span>
+              {type === 'userDetails'
+                ? `By ${user.username}`
+                : `${songs?.length ? songs?.length : ''} songs`}{' '}
+            </span>
           </div>
         </div>
       )}
