4447d8369666c59d97e6a9e6ca04ef1ac38dcd04
aidqen <idanmarkin8@gmail.com>
2024-09-14 16:58:02 +0300

    Implement Loader instead of Loading....

---
+++ src/cmps/Loader.jsx
@@ -0,0 +1,15 @@
+export function Loader() {
+  return (
+    <div className="loader">
+      <div className="backdrop"></div>
+      <div className="equalizer">
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </div>
+  )
+}
--- src/pages/AppIndex.jsx
+++ src/pages/AppIndex.jsx
@@ -2,15 +2,20 @@ import { Outlet } from 'react-router'
 import { AppHeader } from '../cmps/AppHeader'
 import { stationService } from '../services/station/station.service.local'
 import { useSelector } from 'react-redux'
+import { Loader } from '../cmps/Loader'
 
 export function AppIndex() {
   const detailsSidebarMode = useSelector(
     state => state.systemModule.detailsSidebarMode
   )
+  const isLoading = useSelector(state => state.systemModule.isLoading)
 
   return (
     <>
       <main className="main-content">
+        {isLoading &&
+         <Loader />
+          }
         <main className={detailsSidebarMode ? 'open' : ''}>
           {/* <AppHeader /> */}
           <div className="main-section flex flex-column">
