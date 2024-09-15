67e1ddafe7eae1d28358e7f6d719f83418938b7b
aidqen <idanmarkin8@gmail.com>
2024-09-15 19:30:47 +0300

    Remove border from user-watching

--- src/cmps/ArtistList.jsx
+++ src/cmps/ArtistList.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-import { SpotifyAPIService } from '../services/spotifyAPI/spotifyAPI.service';
+import React from 'react'
+import { useNavigate } from 'react-router'
+import { SpotifyAPIService } from '../services/spotifyAPI/spotifyAPI.service'
 
 export function ArtistList({ artists }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    async function navigateToArtist(artistId) {
-        navigate(`/artist/${artistId}`);
-                
-    }
-    
-    return (
-        <div className="artist-list">
-            {artists?.map((artist) => {
-                console.log('artist:', artist)
-                if (artist.images.length === 0) return null; // Correct use of conditional
-                return (
-                    <div key={artist.id} className="artist-item flex flex-column justify-start" onClick={() => navigateToArtist(artist.id)}>
-                        <img src={artist.images[0]?.url || 'default-image-url'} alt={artist.name} />
-                        <div className="artist-info">
-                            <h3>{artist.name}</h3>
-                            <p>{artist.followers.total.toLocaleString()} followers</p>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  async function navigateToArtist(artistId) {
+    navigate(`/artist/${artistId}`)
+  }
+
+  return (
+    <ul className="artist-list">
+      {artists?.map(artist => {
+        console.log('artist:', artist)
+        if (artist.images.length === 0) return null // Correct use of conditional
+        return (
+          <>
+            <li
+              key={artist.id}
+              className="artist-item flex flex-column justify-start"
+              onClick={() => navigateToArtist(artist.id)}
+            >
+              <img
+                src={artist.images[0]?.url || 'default-image-url'}
+                alt={artist.name}
+              />
+              <div className="artist-info">
+                <h3>{artist.name}</h3>
+                <p>{artist.followers.total.toLocaleString()} followers</p>
+              </div>
+            </li>
+          </>
+        )
+      })}
+    </ul>
+  )
 }
